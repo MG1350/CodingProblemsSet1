abe33e4b8129d8f8cb0bf499864e8f0f57f4e76e
MG1350 <114371390+MG1350@users.noreply.github.com>
2024-02-08 14:36:43 -0500

    Update example2.java

--- example2.java
+++ example2.java
@@ -4,7 +4,7 @@ public class example2
     {
         System.out.print("*****************************************");
         System.out.print("\n*\t\tSue's Swimwear\t\t*");
-        System.out.print("\n*\tSwimWithSue@swimwera.com\t*");
+        System.out.print("\n*\tSwimWithSue@swimwear.com\t*");
         System.out.print("\n*****************************************");
     }
 }
