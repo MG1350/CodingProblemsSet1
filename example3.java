360e9d9dabd90a43293e870ae0c38fb1b85c13b0
MG1350 <114371390+MG1350@users.noreply.github.com>
2024-02-08 14:35:31 -0500

    Update example3.java

--- example3.java
+++ example3.java
@@ -2,6 +2,8 @@ public class example3
 {
     public static void main(String[] args)
     {
-        
+        System.out.print("Maverick");
+        System.out.print("\n\nButton: Never Give Up!");
+        System.out.print("\nProverb: Be like water...");
     }   
 }
