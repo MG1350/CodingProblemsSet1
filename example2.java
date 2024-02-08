171af719f57863996e160920bf0637255bbbee46
MG1350 <114371390+MG1350@users.noreply.github.com>
2024-02-08 14:32:40 -0500

    Update example2.java

--- example2.java
+++ example2.java
@@ -2,6 +2,9 @@ public class example2
 {
     public static void main(String[] args)
     {
-        
+        System.out.print("*****************************************");
+        System.out.print("\n*\t\tSue's Swimwear\t\t*");
+        System.out.print("\n*\tSwimWithSue@swimwera.com\t*");
+        System.out.print("\n*****************************************");
     }
 }
